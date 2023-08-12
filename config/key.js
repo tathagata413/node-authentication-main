664e1b81b30e08a9313a4bce4f0ad54c69fd481f
Tathagata Mitra <68333829+tathagata413@users.noreply.github.com>
2023-08-12 19:24:08 +0530

    Node Authentication

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MongoURI:
+    // " mongodb://127.0.0.1:27017/authantication",
+   "mongodb+srv://sheshmani47581:sapY2fVIHmyjj6zP@cluster0.etldepg.mongodb.net/?retryWrites=true&w=majority"
+};
